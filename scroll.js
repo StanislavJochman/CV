916406b9c458bd7bf13e1f9099fa6960061ff087
KatarinaMarcincakova <katulik137@gmail.com>
2020-05-06 02:26:29 +0200

    scroll fixnuty

--- scroll.js
+++ scroll.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-    $('.container').click(function() { 
-      $('body').css('overflow', 'hidden');
-      $('.modal-content').css('overflow-y', 'auto');
-    });
-  $('.close_m').click(function() { 
-      $('body').css('overflow', 'auto');
-      $('.modal-content').css('overflow-y', 'hidden');
-    });
+  $('.container').click(function() { 
+    $('body').css('overflow', 'hidden');
+    $('.modal-content').css('overflow-y', 'auto');
   });
+  $('.close_m').click(function() { 
+    $('body').css('overflow', 'auto');
+    $('.modal-content').css('overflow-y', 'hidden');
+  });
+  $('.close_m_b').click(function() { 
+    $('body').css('overflow', 'auto');
+    $('.modal-content').css('overflow-y', 'hidden');
+   });
+});
