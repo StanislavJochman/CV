7a8e33f03b1fd1bfcbf725f8a81175e38197c4ba
KatarinaMarcincakova <katulik137@gmail.com>
2020-05-16 16:48:13 +0200

    new version

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var m = document.getElementsByClassName("modal");
-//var span = document.getElementsByClassName("close")[0];
+numOfTl = 3;
 
-function close_modal(numM) {
-  m[numM].style.display = "none";
-  //console.log("close")
+$('.aboutNavBtn').eq(0).css("background-color", "#0c1d24")
+
+for (i = 1; i < numOfTl; i++) {
+	$('.tldiv').eq(i).addClass('hidden');
+    $('.aboutNavBtn').eq(i).css("background-color", "transparent")
+    $('.aboutNavBtn').eq(i).css("color", "#e4e3f5")
 }
 
-function open_modal(numM) {
-  m[numM].style.display = "block";
-  //console.log("open")
+function open_tl(num) {
+
+    for (i = 0; i < numOfTl; i++) {
+        $('.tldiv').eq(i).addClass('hidden');
+        $('.aboutNavBtn').eq(i).css("background-color", "transparent")
+        $('.aboutNavBtn').eq(i).css("color", "#e4e3f5")
+    }
+
+    $('.tldiv').eq(num).removeClass('hidden');
+    $('.tldiv').eq(num).addClass('shown');
+    $('.aboutNavBtn').eq(num).css("background-color", "#0c1d24")
 }
-
-
