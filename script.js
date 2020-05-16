8fb7045357f97ebb2269ffaabbaa1f6393cf6f12
KatarinaMarcincakova <katulik137@gmail.com>
2020-05-17 00:04:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-numOfTl = 3;
+// ********************************************************************************* TIMELINES ***************************************************************
+
+var numOfTl = 3;
 
 $('.aboutNavBtn').eq(0).css("background-color", "#0c1d24")
 
@@ -21,19 +23,56 @@ function open_tl(num) {
     $('.aboutNavBtn').eq(num).css("background-color", "#0c1d24")
 }
 
-// var circle = document.querySelector('.Xcircle');
-// var radius = circle.r.baseVal.value;
-// var circumference = radius * 2 * Math.PI;
+// ********************************************************************************* COMPETITIONS ************************************************************
 
-// circle.style.strokeDasharray = `${circumference} ${circumference}`;
-// circle.style.strokeDashoffset = `${circumference}`;
+var numOfComp = 7;
+var numOfRob = 5;
+var numOfInf = 2;
 
-// function setProgress(percent, skill) {
-//     const offset = circumference - percent / 100 * circumference;
-//     circle.style.strokeDashoffset = offset;
-// }
+$('.compNavBtn').eq(0).css("background-color", "#0c1d24")
 
-// setProgress(20);
+for (i = 1; i < 3; i++) {
+    $('.compNavBtn').eq(i).css("background-color", "transparent")
+    $('.compNavBtn').eq(i).css("color", "#e4e3f5")
+}
+
+function open_all_comp() {
+    for (i = 0; i < numOfComp; i++) {
+        $('.comp').eq(i).removeClass('hidden');
+        $('.comp').eq(i).addClass('shown');
+    }
+    $('.compNavBtn').eq(0).css("background-color", "#0c1d24");
+    $('.compNavBtn').eq(1).css("background-color", "transparent");
+    $('.compNavBtn').eq(2).css("background-color", "transparent");
+}
+
+function open_inf_comp() {
+
+    for (i = 0; i < numOfInf; i++) {
+        $('.inf').eq(i).removeClass('hidden');
+        $('.inf').eq(i).addClass('shown');
+    }
+
+    $('.compNavBtn').eq(1).css("background-color", "#0c1d24");
+    $('.compNavBtn').eq(2).css("background-color", "transparent");
+    $('.compNavBtn').eq(0).css("background-color", "transparent");
+}
+
+function open_rob_comp() {
+
+    for (i = 0; i < numOfRob; i++) {
+        $('.rob').eq(i).removeClass('hidden');
+        $('.rob').eq(i).addClass('shown');
+    }
+    
+    $('.compNavBtn').eq(2).css("background-color", "#0c1d24");
+    $('.compNavBtn').eq(1).css("background-color", "transparent");
+    $('.compNavBtn').eq(0).css("background-color", "transparent");
+}
+
+open_all_comp();
+
+// ********************************************************************************* SKILLS *******************************************************************
 
 var listOfSkills = ['.html', '.css', '.js', '.python', '.arduino', '.ne']
 var skillPerc = [80, 80, 70, 80, 60, 95]
