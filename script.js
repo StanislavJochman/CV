20522302a53a4c236007c11d35858d1552c2ad5d
KatarinaMarcincakova <katulik137@gmail.com>
2020-05-20 00:29:19 +0200

    prehodene d0 d1, tlcontent bez final farby

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 numOfTl = 3;
 
 $('.aboutNavBtn').eq(0).css("background-color", "#0c1d24")
+$('.aboutNavBtn').eq(0).css("color", "#e4e3f5")
 
 for (i = 1; i < numOfTl; i++) {
 	$('.tldiv').eq(i).addClass('hidden');
     $('.aboutNavBtn').eq(i).css("background-color", "transparent")
-    $('.aboutNavBtn').eq(i).css("color", "#e4e3f5")
+    $('.aboutNavBtn').eq(i).css("color", "#0c1d24")
 }
 
 function open_tl(num) {
@@ -13,12 +14,14 @@ function open_tl(num) {
     for (i = 0; i < numOfTl; i++) {
         $('.tldiv').eq(i).addClass('hidden');
         $('.aboutNavBtn').eq(i).css("background-color", "transparent")
-        $('.aboutNavBtn').eq(i).css("color", "#e4e3f5")
+        $('.aboutNavBtn').eq(i).css("color", "#0c1d24")
     }
 
     $('.tldiv').eq(num).removeClass('hidden');
     $('.tldiv').eq(num).addClass('shown');
     $('.aboutNavBtn').eq(num).css("background-color", "#0c1d24")
+    $('.aboutNavBtn').eq(num).css("color", "#e4e3f5")
+
 }
 
 var listOfSkills = ['.html', '.css', '.js', '.python', '.arduino', '.ne']
