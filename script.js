c9f1e6f45ba4aa1b9ab8e6accc20b1987db23f0a
KatarinaMarcincakova <marcincakova.katarina@gmail.com>
2021-03-21 12:24:36 +0100

    cs

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ function open_tl(num) {
 // *************************************************************************** SKILLS **********************************
 
 var listOfSkills = ['.html', '.css', '.js', '.python', '.arduino', '.ne', '.cs'];
-var skillPerc = [75, 70, 75, 85, 40, 100, 55];
+var skillPerc = [75, 70, 75, 85, 40, 100, 40];
 var numOfScircles = listOfSkills.length;
 var browserW = $(window).width();
 var gradientStart = '#8cc2d9';
